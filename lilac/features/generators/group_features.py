8f99cbbe23f2c740a688d40dfb41f1b539cd4219
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2022-05-23 21:57:59 +0900

    updated group feature

--- lilac/features/generators/group_features.py
+++ lilac/features/generators/group_features.py
@@ -10,9 +10,10 @@ class GroupFeatures(FeaturesBase):
     なので返す特徴量の個数はlen(input_cols)*(5+3*5)となる.
     """
 
-    def __init__(self, input_cols, group_key,  features_dir=None):
+    def __init__(self, input_cols, group_key, do_add_diff_ratio=True, features_dir=None):
         self.group_key = group_key
         self.input_cols = input_cols
+        self.do_add_diff_ratio = do_add_diff_ratio
         super().__init__(features_dir)
 
     def fit(self, df):
@@ -30,24 +31,27 @@ class GroupFeatures(FeaturesBase):
     def transform(self, df):
         df = pd.merge(df, self._agg, on=self.group_key, how="left")
         aggregated_cols = list(self._agg.columns)
-        for agg_func in ["mean", "max", "min",
-                         "median", "sum"]:
-            df, result_cols = self.add_diff_ratio(df, agg_func)
-            aggregated_cols.extend(result_cols)
+        if self.do_add_diff_ratio:
+            for agg_func in ["mean", "max", "min",
+                             "median", "sum"]:
+                df, result_cols = self.add_diff_ratio(df, agg_func)
+                aggregated_cols.extend(result_cols)
         return df[aggregated_cols].drop(self.group_key, axis=1)
 
     def add_diff_ratio(self, data, agg_func):
         result_cols = []
+        added_df = pd.DataFrame()
         for group_col in self.input_cols:
             agg_col = f"agg_{agg_func}_{group_col}_grpby_{self.group_key}"
-            data[f"{agg_col}_diff"] = data[group_col].values - \
+            added_df[f"{agg_col}_diff"] = data[group_col].values - \
                 data[agg_col].values
-            data[f"{agg_col}_ratio"] = data[group_col].values / \
+            added_df[f"{agg_col}_ratio"] = data[group_col].values / \
                 (data[agg_col]+1e-9)
-            data[f"{agg_col}_diff_ratio"] = data[f"{agg_col}_diff"].values / \
+            added_df[f"{agg_col}_diff_ratio"] = added_df[f"{agg_col}_diff"].values / \
                 (data[agg_col]+1e-9)
             result_cols.extend(
                 [f"{agg_col}_diff", f"{agg_col}_ratio", f"{agg_col}_diff_ratio"])
+        data = pd.concat([data, added_df], axis=1)
         return data, result_cols
 
     class MaxMin:
---
+++ projects/sony/list_scores.py
@@ -0,0 +1,12 @@
+import glob
+from pathlib import Path
+import json
+output_path_list = glob.glob("data/output/*.json")
+
+
+for output_path in sorted(output_path_list):
+    p = Path(output_path)
+    with p.open("r") as f:
+        data = json.load(f)
+        score = data["output"].get("score")
+        print(f"{p.stem}: {score}")
